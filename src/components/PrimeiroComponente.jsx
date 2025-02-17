328cc904610d15a87767a50bda410edffd968b91
EA - Pablo Almeida Da Silva <pablo_silva@educadventista.org>
2025-02-17 08:27:42 -0300

    Aula do dia 17/02/2025

--- src/components/PrimeiroComponente.jsx
+++ src/components/PrimeiroComponente.jsx
@@ -1,7 +1,9 @@
 export  function PrimeiroComponente() {
   return (
     <div>
-      <h1>hugo</h1>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
     </div>
   )
 }
